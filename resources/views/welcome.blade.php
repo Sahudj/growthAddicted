58b87df0dc394a476035e0b4cbabe9e93c24f967
dhananjay sahu <sahubhupend082@gmail.com>
2024-02-04 18:19:09 +0530

    feb4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,14 +42,98 @@
                     <div class="hm-right-wrap">
                         <img class="img1 layer" data-speed="4" src="{{url('public/frontend/home/')}}/assets/images/section1-img2.png" alt="">
                         <img class="img2" src="{{url('public/frontend/home/')}}/assets/images/section1-img3.png" alt="">
-                        <img class="img3 layer" data-speed="8" src="{{url('public/frontend/home/')}}/assets/images/section1-img4.png" alt="">
+                        <!-- <img class="img3 layer" data-speed="8" src="{{url('public/frontend/home/')}}/assets/images/section1-img4.png" alt=""> -->
 
                     </div>
                 </div>
             </div>
         </div>
+        <!-- UPCOMING PROGRAM  -->
+        <div class="up-section">
 
+            <h1 class="heading-with-bar">UPCOMING PROGRAMS</h1>
+            <div class="section2-cont">
+                <div class="up-section-left">
+                    <img src="{{url('public/frontend/home/')}}/assets/images/standard-course.png" alt="">
+                </div>
+                <div class="up-section-right">
+                    <h1>Target Mastery</h1>
+                    <div class="right-bottom">
+                        <div class="img-box">
+                            <img src="{{url('public/frontend/home/')}}/assets/images/3d-img/dummy-img.png" alt="">
+                        </div>
+                        <div class="right-bottom-right">
+                            <div class="text-contain"><span>IGNITE YOUR INTELLECT:</span> Unleash the Power of Education at Growth Addicted Where Marketing Excellence Begins!</div>
+                            <div class="name-contain">BY : AMAN UPADHYAY</div>
+                            <a href="" class="explore-contain"> EXPLORE ---- <span class="material-symbols-outlined">
+                                    keyboard_arrow_right
+                                </span></a>
+                        </div>
+                    </div>
 
+                </div>
+            </div>
+
+        </div>
+        <!-- OUR DIGITAL CLUSTERS   -->
+        <div class="our-dig-cluster-section">
+            <div class="our-dig-inner">
+                <div class="div-cont">
+                    <h1 class="heading-with-bar-right"> OUR DIGITAL CLUSTERS </h1>
+                    <p style="margin-top:30px;font-weight:500; font-size:1.5em">Go beyond the digital realm</p>
+                </div>
+                <div class="cluster-section-cont">
+                    <?php
+                    $packageId = '';
+                    $orderStatus = '';
+                    ?>
+
+                    <?php
+
+                    function encryptor($action, $string)
+                    {
+                        $output = false;
+
+                        $encrypt_method = "AES-256-CBC";
+                        //pls set your unique hashing key
+                        $secret_key = 'aman#$gyan13*&som@$#';
+                        $secret_iv = 'aman#$gyan13*&som@$#';
+
+                        // hash
+                        $key = hash('sha256', $secret_key);
+
+                        // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+                        $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+                        //do the encyption given text/string/number
+                        if ($action == 'encrypt') {
+                            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+                            $output = base64_encode($output);
+                        } else if ($action == 'decrypt') {
+                            //decrypt the given text/string/number
+                            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+                        }
+
+                        return $output;
+                    }
+                    $allPackages = DB::table('packages')->select('*')->where('status', 1)->get();
+
+                    ?>
+                      @auth()
+                    
+                    <?php
+                        $packageId = (auth()->user()->package_id) ? auth()->user()->package_id : 0;
+                        $orderStatus = auth()->user()->order_status;
+                        $packages = DB::table('packages')->select(DB::raw('group_concat(id) as packageId'))->where('id', '>', $packageId)->first(); 
+                        $tempArr = !empty($packages->packageId) ? explode(',', $packages->packageId) : [] ;
+                    ?>
+                @endauth()
+                    <div class="cluster-section-inner">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <!-- banner section end -->
