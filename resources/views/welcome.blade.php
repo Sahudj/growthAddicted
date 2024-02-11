7545eef297e685fc8b351f3ea3d4c4fe013cb1b3
dhananjay sahu <sahubhupend082@gmail.com>
2024-02-11 16:02:18 +0530

    11feb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,112 +17,11 @@
         <!-- HEADER SECTION  -->
         <div class="home-header">
             <div class="home-header-inner">
-                <div class="home-header-left">
-                    <div class="hm-left-wrap">
-                        <img class="layer" data-speed="-5" src="{{url('public/frontend/home/')}}/assets/images/section1-img1.png" alt="">
-                        <div class="section1-heading " data-speed="2">
-                            NAVIGATE THE<br> DIGITAL <br> FRONTIER
-                        </div>
-                        <div class="section1-btn-cont">
-                            <a class="btn-cont-left" href="">GET STARTED <span style="margin-left: 10px;" class="material-symbols-outlined">
-                                    east
-                                </span></a>
-                            <a class="btn-cont-right" href="">
-                                <div class="circle">
-
-                                    <span class="material-symbols-outlined">
-                                        play_arrow
-                                    </span>
-                                </div> WATCH VIDEO
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="home-header-right">
-                    <div class="hm-right-wrap">
-                        <img class="img1 layer" data-speed="4" src="{{url('public/frontend/home/')}}/assets/images/section1-img2.png" alt="">
-                        <img class="img2" src="{{url('public/frontend/home/')}}/assets/images/section1-img3.png" alt="">
-                        <!-- <img class="img3 layer" data-speed="8" src="{{url('public/frontend/home/')}}/assets/images/section1-img4.png" alt=""> -->
-
-                    </div>
-                </div>
+              
             </div>
         </div>
-        <!-- UPCOMING PROGRAM  -->
-        <div class="up-section">
+       
 
-            <h1 class="heading-with-bar">UPCOMING PROGRAMS</h1>
-            <div class="section2-cont">
-                <div class="up-section-left">
-                    <img src="{{url('public/frontend/home/')}}/assets/images/standard-course.png" alt="">
-                </div>
-                <div class="up-section-right">
-                    <h1>Target Mastery</h1>
-                    <div class="right-bottom">
-                        <div class="img-box">
-                            <img src="{{url('public/frontend/home/')}}/assets/images/3d-img/dummy-img.png" alt="">
-                        </div>
-                        <div class="right-bottom-right">
-                            <div class="text-contain"><span>IGNITE YOUR INTELLECT:</span> Unleash the Power of Education at Growth Addicted Where Marketing Excellence Begins!</div>
-                            <div class="name-contain">BY : AMAN UPADHYAY</div>
-                            <a href="" class="explore-contain"> EXPLORE ---- <span class="material-symbols-outlined">
-                                    keyboard_arrow_right
-                                </span></a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-        </div>
-        <!-- OUR DIGITAL CLUSTERS   -->
-        <div class="our-dig-cluster-section">
-            <div class="our-dig-inner">
-                <div class="div-cont">
-                    <h1 class="heading-with-bar-right"> OUR DIGITAL CLUSTERS </h1>
-                    <p style="margin-top:30px;font-weight:500; font-size:1.5em">Go beyond the digital realm</p>
-                </div>
-
-            </div>
-            <!-- backgound section3 -->
-            <div class="section3-background">
-                <div class="box-cont">
-                    <div class="box1"></div>
-                    <div class="box2"></div>
-                    <div class="box3"></div>
-                    <div class="box4"></div>
-                </div>
-            </div>
-        </div>
-        <!-- OUR MISSION SECTION   -->
-        <div class="our-mission-section">
-            <img class="layer img1" data-speed="-8" src="{{url('public/frontend/home/')}}/assets/images/section1-img1.png" alt="">
-            <img class="layer img2" data-speed="-2" src="{{url('public/frontend/home/')}}/assets/images/section1-img1.png" alt="">
-            <img class="layer img3" data-speed="-4" src="{{url('public/frontend/home/')}}/assets/images/section1-img1.png" alt="">
-            <div class="inner-section">
-                <h1 class="heading-with-bar-right-white" style="color:#fff"> OUR DIGITAL CLUSTERS </h1>
-                <div class="din-content">
-                    Growth Addtcted Mission Is To Help Learners Of All Ages Reach Their Full Potential Through Inspired Teaching And Personalized Learning. We Do This By Providing Clear Pathways For Learners To Expand Their Skills, Explore Their Options, And Change Their Lives
-                </div>
-            </div>
-        </div>
-        <!-- DISCOVER US ANY THING ANY TIME -->
-        <div class="discover-us-section">
-            <img class=" phone-img" src="{{url('public/frontend/home/')}}/assets/images/phone1.png" alt="">
-            <img class=" laptop-img" src="{{url('public/frontend/home/')}}/assets/images/laptop.png" alt="">
-            <img class=" logo-img" src="{{url('public/frontend/home/')}}/assets/images/Logo2.png" alt="">
-
-            <h1 class="heading">DISCOVER US <br>
-                ANY TIME ANY WHERE</h1>
-            <div class="inner-section-du">
-                <div class="text-cont">
-                    <p>
-                        Our platform is designed for seamless access, ensuring you can embark on your journey of discovery whenever it suits you. No matter the time zone or location, we're here for you.
-                        📱 Mobile-Friendly: Take your
-                    </p>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 <!-- banner section end -->
