d0944a810f67c05ea2e142f3fee68472235badfd
dhananjay sahu <sahubhupend082@gmail.com>
2024-02-12 23:59:26 +0530

    sdfgwfg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,9 +16,20 @@
     <div class="home-page-wrap">
         <!-- HEADER SECTION  -->
         <div class="home-header">
-            <div class="home-header-inner">
-              
+           <div class="home-header-top">
+            <div class="top-hd-bg"></div>
+            <div class="top-hd-fg">
+                <h3>TOP WEB DESIGN AGENCY</h3>
+                <div class="fg-title">
+                    <h1>WE GROW BRANDS ONLINE</h1>
+                    <h4>CUSTOM WEBSITES, BRANDING & DIGITAL MARKETING SOLUTIONS</h4>
+                </div>
+                <div>
+                    <a href="#">Get Started</a>
+                </div>
             </div>
+           </div>
+           <div class="home-header-bottom"></div>
         </div>
        
 
